a99d8dbf296a291eaa9904c746b921c801988a8e
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2024-07-04 20:22:00 +0800

    allow rotating text, change text display style

--- demo_app.py
+++ demo_app.py
@@ -1,15 +1,57 @@
 import streamlit as st 
-from PIL import Image 
+from PIL import Image, ImageChops 
 
 import image_text_replacer 
 
 st.header("Upload your image here to test the text replacer!")
 
+init_session_state_values = {
+    'ress': None, 
+    'image': None, 
+    'show_bg': True, 
+    'black_text': True, 
+    'i': 0, 
+}
+for k, v in init_session_state_values.items(): 
+    if k not in st.session_state: 
+        st.session_state[k] = v 
+        
+
 image_uploader = st.file_uploader("", type=["jpg","png","jpeg","webp"])
 if image_uploader is not None:
     image = Image.open(image_uploader)
+    if (not st.session_state['image']) or (not (ImageChops.difference(st.session_state['image'], image)) ): # if it's a new images 
+        ress = image_text_replacer.get_texts_bboxes_dirns(image) 
+        st.session_state['image'] = image 
+        st.session_state['ress'] = ress 
+    else: 
+        ress = st.session_state['ress'] 
+
     st.image(image, "original image")
-    replaced_image = image_text_replacer.replace_image(image) 
-    st.image(replaced_image, "replaced image")
+    
+
+    def toggle_bg(): 
+        st.session_state['show_bg'] = not st.session_state['show_bg'] 
+        print(st.session_state['show_bg'])
+        draw_replaced_image() 
+    
+    def toggle_black_text(): 
+        st.session_state['black_text'] = not st.session_state['black_text'] 
+        print(st.session_state['black_text'])
+        draw_replaced_image() 
+
+    def draw_replaced_image(): 
+        st.session_state['i'] += 2 
+        st.button('Hide background' if st.session_state['show_bg'] else 'Show background', "s/h bg"+str(st.session_state['i']), on_click=toggle_bg)
+        st.button('Switch to white text' if st.session_state['black_text'] else 'Switch to black text', "b/w text"+str(st.session_state['i']), on_click=toggle_black_text)
+        replaced_image = image_text_replacer.redraw_img(image, ress, st.session_state['show_bg'], st.session_state['black_text'])
+        st.image(replaced_image, "replaced image")
+    
+    draw_replaced_image() 
+
+    
+
+    
+    
 
 
--- image_text_replacer.py
+++ image_text_replacer.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw, ImageFont
 from google.cloud import vision 
   
 
-def OpenDyslexicFontFitSize(text, w, h): # size limit in pixels
+def OpenDyslexicFontFitSize(text, w, h, return_bbox=False): # size limit in pixels
     fontsize = 1
     font = ImageFont.truetype("OpenDyslexic_0_91_12_regular.otf", fontsize)
     bbox = font.getbbox(text) # (left, top, right, bottom)
@@ -35,6 +35,9 @@ def OpenDyslexicFontFitSize(text, w, h): # size limit in pixels
     #print(w, bbox[2]-bbox[0])
     #print(h, bbox[3]-bbox[1])
 
+    if return_bbox: 
+        return font, font.getbbox(text) 
+
     return font
 
 def ltrb_to_midxmidywh(left, top, right, bottom): # (midx, midy, width, height) 
@@ -45,9 +48,7 @@ def PIL_to_bytearray(img:Image.Image):
     img.save(img_byte_arr, format='PNG') 
     return img_byte_arr.getvalue() 
 
-def get_text_bboxes(img:Image.Image): 
-    # TODO: use Google Vision API to detect the text in the image 
-    # i think helpful Google Vision API docs link: https://cloud.google.com/vision/docs/ocr#vision_text_detection-python 
+def get_texts_bboxes_dirns(img:Image.Image): 
 
     client = vision.ImageAnnotatorClient() 
     image = vision.Image(content=PIL_to_bytearray(img)) 
@@ -61,48 +62,103 @@ def get_text_bboxes(img:Image.Image):
     texts = response.text_annotations 
 
     ress = [] 
-    skippedfirst = False 
     for text in texts: 
-        if not skippedfirst: 
-            skippedfirst = True 
+        if '\n' in text.description: # multiline will be detected twice, so skip it. 
             continue 
+        
+
+        # get bbox that's guaranteed to fit 
+
         xs = [] 
         ys = [] 
-        for vertex in text.bounding_poly.vertices: 
+        for vertex in text.bounding_poly.vertices: # the first one will be the top left 
             xs.append(vertex.x) 
             ys.append(vertex.y) 
         
         sxs = sorted(xs) 
         sys = sorted(ys) 
         ltrb = sxs[1], sys[1], sxs[2], sys[2] # bbox that's guaranteed to fit 
-        ress.append((text.description, ltrb)) 
+
+
+        # get direction 
+        tl = text.bounding_poly.vertices[0] 
+        is_right = ( abs(tl.x-ltrb[0]) > abs(tl.x-ltrb[2]) ) # if it's closer to rightmost 
+        is_bottom = ( abs(tl.y-ltrb[1]) > abs(tl.y-ltrb[3]) ) # if it's closer to bottommost 
+        if ( (not is_right) and (not is_bottom) ): 
+            dirn = 0 # upright 
+        elif is_right: 
+            if is_bottom: 
+                dirn = 2 # upside down 
+            else: 
+                dirn = 1 # rotated clockwise 90 deg 
+        else: 
+            dirn = 3 
+
+        #print('"'+text.description+'":',dirn, ltrb, tl) 
+
+        ress.append((text.description, ltrb, dirn)) 
+
     return ress 
 
 
-def replace_image(img:Image.Image): 
+def redraw_img(img_in, ress, show_bg=True, black_text=True): 
+    img = img_in.convert("RGBA") # also makes a copy so edits can be done without hesitation 
 
-    ress = get_text_bboxes(img) # each bbox is (left, top, right, bottom)
+    if show_bg: 
+        draw = ImageDraw.Draw(img) 
+
+        # draw all the rectangles first 
+        for (text, ltrb, dirn) in ress: 
+
+            # TODO: account for bg colour? or even remove the text and try to recover background maybe 
+            draw.rectangle( ltrb , fill="white" if black_text else "black", outline=None ) 
     
 
-    draw = ImageDraw.Draw(img) 
-
-    # draw all the rectangles first 
-    for (text, ltrb) in ress: 
-
-        # TODO: account for bg colour? or even remove the text and try to recover background maybe 
-        draw.rectangle( ltrb , fill="white", outline=None ) 
-
-
     # then draw the text 
-    for (text, ltrb) in ress: 
+    for (text, ltrb, dirn) in ress: 
 
-        bbox = ltrb_to_midxmidywh(*ltrb) 
+        max_bbox = ltrb_to_midxmidywh(*ltrb) 
 
-        #print("LTRB:",ltrb)
-        #print("BBOX:",bbox)
+        if dirn%2 == 0: 
+            max_textimgsize = (max_bbox[2], max_bbox[3])
+        else: 
+            max_textimgsize = (max_bbox[3], max_bbox[2])
 
         # TODO: account for text colour? 
-        draw.text( (ltrb[0], ltrb[1]) , text , font=OpenDyslexicFontFitSize(text, bbox[2], bbox[3]) , fill='black') 
+        font, bbox = OpenDyslexicFontFitSize(text, *max_textimgsize, return_bbox=True) 
+        # the final size is bbox[2], bbox[3]... though it might be rotated 
+
+        if dirn%2 == 0: 
+            textimgsize = (bbox[2], bbox[3])
+        else: 
+            textimgsize = (bbox[3], bbox[2])
+        
+        larger = max(textimgsize) 
+        
+        textimg = Image.new('RGBA', (larger, larger), (255,255,255,0)) 
+        tid = ImageDraw.Draw(textimg) 
+        
+        tid.text((0,0), text, font=font, fill='black' if black_text else 'white') 
+        rot_textimg = textimg.rotate(-90*dirn).crop(((larger-textimgsize[0]) if dirn==1 else 0, 
+                                                     (larger-textimgsize[1]) if dirn==2 else 0, 
+                                                     textimgsize[0] if dirn==3 else larger, 
+                                                     textimgsize[1] if dirn==0 else larger)) 
+
+        center_offset_x = (max_bbox[2] - textimgsize[0])//2 
+        center_offset_y = (max_bbox[3] - textimgsize[1])//2 
+
+        img.paste( rot_textimg , (ltrb[0]+center_offset_x, ltrb[1]+center_offset_y) , rot_textimg ) 
+
     return img 
+
+
+
+def replace_image(img_in:Image.Image): 
+
+    
+    ress = get_texts_bboxes_dirns(img_in) # each bbox is (left, top, right, bottom)
+
+    return redraw_img(img_in, ress, show_bg=True, black_text=True), ress # to allow redrawing with different parameters 
+
 
 
